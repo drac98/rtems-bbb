4f6eb4b3ec7c92bec9d1fe557a61483c07f3865a
Christian Mauderer <christian.mauderer@embedded-brains.de>
2018-09-27 11:57:50 +0200

    Remove everything for beagle.

--- build/configuration.sh
+++ build/configuration.sh
@@ -2,26 +2,17 @@
 # be already set:
 #   $PROJECTDIR -- set to the base dir of the project
 
-BSP_NAME="beagleboneblack"
+BSP_NAME="xilinx_zynq_a9_qemu"
 RTEMS_CPU="arm"
 RTEMS_VERSION="5"
 TARGET="${RTEMS_CPU}-rtems${RTEMS_VERSION}"
 PREFIX="${PROJECTDIR}/install/rtems/${RTEMS_VERSION}/"
-UBOOT_CONFIG="am335x_evm"
-DTB_FILE="src/arm/am335x-boneblack.dtb"
-DTB_INSTALL_NAME="am335x-boneblack.dtb"
 
 RSB_DIR="${PROJECTDIR}/tools/rtems-source-builder"
 RTEMS_SOURCE_DIR="${PROJECTDIR}/libs/rtems"
 LIBBSD_SOURCE_DIR="${PROJECTDIR}/libs/rtems-libbsd"
-U_BOOT_SOURCE_DIR="${PROJECTDIR}/tools/u-boot"
-NEWFS_MSDOS_SOURCE_DIR="${PROJECTDIR}/tools/newfs_msdos"
-DEVICETREE_DIR="${PROJECTDIR}/tools/device-tree-rebasing"
-MTOOLS_DIR="${PROJECTDIR}/tools/mtools"
-PARTITION_DIR="${PROJECTDIR}/tools/partition"
 
 BSP_CONFIG_OPT="
 	--enable-tests=samples
 	--disable-networking
-	CONSOLE_POLLED=1
 	"
