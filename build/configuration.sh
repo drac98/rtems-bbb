ee4990daff2fe2a7362b73c3ed583dc0d30aa099
Christian Mauderer <oss@c-mauderer.de>
2020-03-22 20:55:33 +0100

    Add devicetree for RPi2.

--- build/configuration.sh
+++ build/configuration.sh
@@ -7,10 +7,14 @@ RTEMS_CPU="arm"
 RTEMS_VERSION="5"
 TARGET="${RTEMS_CPU}-rtems${RTEMS_VERSION}"
 PREFIX="${PROJECTDIR}/install/rtems/${RTEMS_VERSION}/"
+DTS_FILE="sys/gnu/dts/arm/bcm2836-rpi-2-b.dts"
+DTB_INSTALL_NAME="bcm2836-rpi-2-b.dtb"
 
 RSB_DIR="${PROJECTDIR}/tools/rtems-source-builder"
 RTEMS_SOURCE_DIR="${PROJECTDIR}/libs/rtems"
 LIBBSD_SOURCE_DIR="${PROJECTDIR}/libs/rtems-libbsd"
+DEVICETREE_DIR="${PROJECTDIR}/tools/devicetree-freebsd-export"
+DEVICETREEOVERLAY_DIR="${PROJECTDIR}/tools/devicetree-overlays"
 
 BSP_CONFIG_OPT="
 	--enable-tests=samples
